75dfaf8773006dc8731bada462611f2f9b40b628
Ravenclaw676 <39599752+Ravenclaw676@users.noreply.github.com>
2023-04-24 08:16:36 +0000

    pain

--- OOP/projects/sinking_ships/main.py
+++ OOP/projects/sinking_ships/main.py
@@ -3,12 +3,26 @@ class Board(object):
         print("no")
 
 class Player (object):
-    def __init__(self, number: int, board: Board):
-        self._board = board
+    def __init__(self, number: int, game_board: Board):
+        self._board = game_board
         self._number = number
+
+    def get_number(self) -> int:
+        return self._number
 
     def get_board(self) -> Board:
         return self._board
+
+    def _get_row(self) -> int:
+        return 0
+
+    def _get_column(self) -> int:
+        return 0
+
+    def take_shot(self):
+        x = self._get_row()
+        y = self._get_column()
+        self._board.take_shot(x, y)
 
 
 board = Board()
