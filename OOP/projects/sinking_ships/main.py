9950156e26bb73a492cfc033726a647e46810a68
Ravenclaw676 <39599752+Ravenclaw676@users.noreply.github.com>
2023-04-24 20:30:11 +0100

    just pain

--- OOP/projects/sinking_ships/main.py
+++ OOP/projects/sinking_ships/main.py
@@ -1,6 +1,42 @@
 class Board(object):
-    def __init__(self):
-        print("no")
+    def __init__(self, columns: int, rows: int, player_number: int):
+        self.__columns = columns
+        self.__rows = rows
+        self.__player_number = player_number
+        self.__board = []
+
+        for row in range(0, rows - 1):
+            self.__board.append([])
+            for column in range(0, columns, - 1):
+                self.__board[row].append("E")
+
+    def get_width(self) -> int:
+        return self.__columns
+    
+    def get_highs(self) -> int:
+        return self.__rows
+    
+    def take_shot(self, x: int, y: int) -> bool:
+        match self.__board[y][x]:
+            case "E":
+                self.__board[y][x] = "M"
+                return True
+            case "S":
+                self.__board[y][x] = "H"
+                return True
+            case _:
+                return False
+            
+    def place_ship(self, length: int, x: int, y: int, direction: str):
+        if direction == "h":
+            for i in range(0, length - 1):
+                if self.__board[y][x + i] == "S":
+                    raise Exception
+                else:
+                    self.__board[y][x + i] = "S"
+        else:
+            for i in range(0, length - 1):
+
 
 
 class Player (object):
@@ -25,11 +61,50 @@ class Player (object):
         y = self._get_column()
         self._board.take_shot(x, y)
 
+    def _place_ships(self):
+        pass
+
+
 class human_player(Player):
     def _get_row(self) -> int:
         row = -1
         while row not in range(0, self._board.get_height() - 1):
-            row = int(input(f"Please enter the row you want to shoot {self._board.get_height()}")) - 1
+            row = int(input(f"Please enter the row you want to shoot \
+                                        {self._board.get_height()}")) - 1
+
+        return row
+
+    def _get_column(self) -> int:
+        column = -1
+        while column not in range(0, self._board.get_width() - 1):
+            column = int(input(f"Please enter the column you want to shoot \
+                                        {self._board.get_width()}")) - 1
+        return column
+
+    def take_shot(self):
+        while True:
+            x = self._get_row()
+            y = self._get_column()
+            if not self._board.take_shot(x, y):
+                print("you can't shoot somewhere you have already shot.")
+            else:
+                break
+
+    def _place_ships(self):
+        for ship in range(2, 5):
+            error = True
+            while error:
+                direction = ""
+                while direction not in ["h", "v"]:
+                    direction = input("What direction is the ship facing,\
+                                      horizontal or vertical (h/v)").lower()
+                x = self._get_column()
+                y = self._get_row()
+                try:
+                    self._board.place_ship(ship, x, y, direction)
+                except Exception:
+                    print("Ships can not overlap, try again")
+                error = False
 
 
 board = Board()
